411d9b53b1a3e9cefdee90b1f928333bcaa93d09
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-08-19 11:19:00 -0300

    feat: create teams and matches migrations

---
+++ app/backend/src/database/migrations/20220819140344-matches.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      homeTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'home_team',
+        eferences:{
+          model: 'Team',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      homeTeamGoals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team',
+        eferences:{
+          model: 'Team',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      awayTeamGoals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team_goals',
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      inProgress: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        field: 'in_progress',
+        defaultValue: 0
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
