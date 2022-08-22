5c266b76a72d011caec85f954601d16306e6cb43
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-08-22 14:48:22 -0300

    feat: create login controller

---
+++ app/backend/src/controllers/login.controller.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import LoginService from '../services/login.services';
+
+export default class LoginController {
+  constructor(private loginService = new LoginService()) {
+  }
+
+  public static validateLogin(req: Request, res: Response) {
+    const { user } = req.body;
+    return res.status(200).json({ role: user.data.role });
+  }
+
+  public async login(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const { email, password } = req.body;
+      const data = await this.loginService.login(email, password);
+      return res.status(200).json({ token: data });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
