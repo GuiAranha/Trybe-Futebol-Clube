d5b51524850a269f7a1f08f4c46c5f0fa292fbe5
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-08-22 14:30:47 -0300

    feat: create login service

---
+++ app/backend/src/services/login.services.ts
@@ -0,0 +1,29 @@
+import * as joi from 'joi';
+import JWT from '../utils/jwt';
+import Users from '../database/models/user';
+import BaseError from '../utils/baseError';
+
+export default class LoginService {
+  constructor(private model = Users) {
+    this.model = model;
+  }
+
+  static validateLogin(email: string, password: string) {
+    const schema = joi.object({
+      email: joi.string().required(),
+      password: joi.string().required(),
+    }).validate({ email, password });
+
+    if (schema.error) {
+      throw new BaseError(400, 'All fields must be filled');
+    }
+  }
+
+  public async login(email: string, password: string): Promise<string | void> {
+    LoginService.validateLogin(email, password);
+    const data = await this.model.findOne({ where: { email } });
+    if (!data) throw new BaseError(401, 'Incorrect email or password');
+    const token = JWT.generateToken(data);
+    return token;
+  }
+}
